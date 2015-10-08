8617adb56875989b177dc0f749da5324e8ac3754
Tesla Lee <leechee89@gmail.com>
2015-10-08 17:26:33 +0800

    fix too many request

--- lib/rails-assets-for-upyun.rb
+++ lib/rails-assets-for-upyun.rb
@@ -3,19 +3,30 @@ require 'uri'
 class RailsAssetsForUpyun
   def self.publish(bucket, username, password, bucket_path="/", localpath='public', upyun_ap="http://v0.api.upyun.com")
     # http://stackoverflow.com/questions/357754/can-i-traverse-symlinked-directories-in-ruby-with-a-glob
+
+    puts "Start time...#{Time.now}"
+    last_request = nil
     Dir[File.join localpath, "**{,/*/**}/*"].select{|f| File.file? f}.each do |file|
-      
       url = URI.encode "/#{bucket}#{bucket_path}#{file[localpath.to_s.size + 1 .. -1]}"
       date = Time.now.httpdate
+
+      # add to avoid too many request error for upyun
+      puts (Time.now.to_f - last_request) if last_request
+      if last_request && (Time.now.to_f - last_request) < 1.0/20.0
+        sleep(1.0/20.0)
+      end
+
+      last_request = Time.now.to_f
       size = RestClient.head("#{upyun_ap}#{url}", {\
-          Authorization: "UpYun #{username}:#{signature 'HEAD', url, date, 0, password}", 
+          Authorization: "UpYun #{username}:#{signature 'HEAD', url, date, 0, password}",
           Date: date}) do |response, request, result, &block|
-        case response.code 
+        case response.code
         when 200
           response.headers[:x_upyun_file_size].to_i
         when 404
           "non-exists"
         else
+          puts "End time...#{Time.now}"
           response.return!(request, result, &block)
         end
       end
@@ -24,8 +35,10 @@ class RailsAssetsForUpyun
       else
         file_content = File.read(file)
         puts "uploading #{size} => #{file_size} #{file}.."
+
+
         RestClient.put("#{upyun_ap}#{url}",  file_content,{\
-          Authorization: "UpYun #{username}:#{signature 'PUT', url, date, file_size, password}", 
+          Authorization: "UpYun #{username}:#{signature 'PUT', url, date, file_size, password}",
           Date: date,
           mkdir: 'true',
           Content_MD5: Digest::MD5.hexdigest(file_content),
